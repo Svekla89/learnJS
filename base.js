64c6a62a97c349853f94ce70501e403be2e489a3
Svekla89 <ivanovskaya89@gmail.com>
2020-07-29 09:07:22 +0300

    Base12

--- base.js
+++ base.js
@@ -2,7 +2,7 @@
 
 //Даны два числа a и b. Найти их среднее арифметическое: "(a + b)/2
 
-const {getRandomIntInterval} = require('../utils')
+const {getRandomIntInterval} = require('./utils')
 
 const a = getRandomIntInterval(0, 10)
 const b = getRandomIntInterval(0, 10)
